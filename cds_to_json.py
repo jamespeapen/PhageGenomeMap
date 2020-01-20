dec56f9d322b34b5b4af038b2a4e8b56ad8550e0
James Eapen <jpe4@students.calvin.edu>
2020-01-20 09:11:36 -0500

    added length support to gene object

--- cds_to_json.py
+++ cds_to_json.py
@@ -5,24 +5,26 @@ import json
 """Gene class
 @params: locus_tag, gene number, start base number, end base number, product name
 """
+
+
 class Gene:
-    def __init__(self, locus_tag, gene_number, start_location, end_location, product):
+    def __init__(self, locus_tag, gene_number, start_location, end_location, length, product):
         self.locus_tag = locus_tag
         self.gene_number = gene_number
         self.start_location = start_location
         self.end_location = end_location
+        self.length = length
         self.product = product
 
     def __str__(self):
         return "gene number: {0}, start: {1}, end: {2}, product: {3}".format(self.gene_number, self.start_location, self.end_location, self.product)
 
 
-
-gene_numbers = []           #list to store gene numbers
-locus_tags = []             #list to store gene locus tags
-start_numbers = []          #gene start base numbers
-end_numbers = []            #gene end base numbers         
-products = []               #protein names
+gene_numbers = []  # list to store gene numbers
+locus_tags = []  # list to store gene locus tags
+start_numbers = []  # gene start base numbers
+end_numbers = []  # gene end base numbers
+products = []  # protein names
 
 
 lines = []
@@ -41,11 +43,11 @@ with open(path) as cds:
 for line in lines:
     if line[0] == "CDS":
         if line[1] == "complement":
-            start_numbers.append(line[2])
-            end_numbers.append(line[3])
+            start_numbers.append(int(line[2]))
+            end_numbers.append(int(line[3]))
         else:
-            start_numbers.append(line[1])
-            end_numbers.append(line[2])
+            start_numbers.append(int(line[1]))
+            end_numbers.append(int((line[2])))
 
     elif len(line) > 1 and line[1] == "locus_tag":
         locus_tags.append(line[2])
@@ -54,22 +56,25 @@ for line in lines:
         gene_numbers.append(line[2])
 
     elif len(line) > 1 and line[1] == "product":
-        product_name = " ".join(line[2:])
+        product_name=" ".join(line[2:])
         products.append(product_name)
 
-#check that all lists match the number of genes
-assert(len(locus_tags) == len(start_numbers) == len(end_numbers) == len(gene_numbers) == len(products))
+# check that all lists match the number of genes
+assert(len(locus_tags) == len(start_numbers) == len(
+    end_numbers) == len(gene_numbers) == len(products))
 print("Number of genes:")
-print(len(locus_tags), len(start_numbers), len(end_numbers), len(gene_numbers), len(products))
+print(len(locus_tags), len(start_numbers), len(
+    end_numbers), len(gene_numbers), len(products))
 
-filename_split = re.split('\W+', filename)
-new_filename = filename_split[0] + ".json"
-new_path = 'gene_jsons/' + new_filename
+filename_split=re.split('\W+', filename)
+new_filename=filename_split[0] + ".json"
+new_path='gene_jsons/' + new_filename
 
-#make gene objects and dump json to file
+# make gene objects and dump json to file
 with open(new_path, 'w+') as file:
     for i in range(0, len(gene_numbers)):
-        gene = Gene(locus_tags[i], gene_numbers[i], start_numbers[i], end_numbers[i], products[i])
+        gene=Gene(locus_tags[i], gene_numbers[i], start_numbers[i],
+                  end_numbers[i], end_numbers[i] - start_numbers[i], products[i])
         file.write(json.dumps(gene.__dict__, indent=4))
 
 file.close()
