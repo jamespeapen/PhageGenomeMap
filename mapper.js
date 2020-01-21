562c7bb2dc589394ceddc8e34c4fd66a2b9fdbec
James Eapen <jpe4@students.calvin.edu>
2020-01-21 14:21:00 -0500

    docs, reorganization

--- mapper.js
+++ mapper.js
@@ -1,3 +1,4 @@
+//margins
 const margin = { top: 50, right: 50, bottom: 50, left: 30 };
 
 const width = 10000;
@@ -5,6 +6,7 @@ const height = 80;
 
 const svg = d3.select("svg#map-area");
 
+//main svg
 svg
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
@@ -13,24 +15,24 @@ svg
   .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
   .style("background-color", "skyblue");
 
-let loadingIndicator = d3.select("h4#loading-indicator").text("Load file");
-
 d3.json("gene_jsons/Barb_flat.json").then(function(genome) {
   loadingIndicator.text("File loaded");
   console.log("Loaded");
   drawMap(genome);
 });
 
+let loadingIndicator = d3.select("h4#loading-indicator").text("Load file");
 console.log("Loading");
 loadingIndicator.text("Loading");
-let xScale;
+
+//x axis scale
 function drawMap(genome) {
   let start_locations = genome.map(d => d.start_location);
   let end_locations = genome.map(d => d.end_location);
   let gene_lengths = genome.map(d => d.length);
   let products = genome.map(d => d.product);
 
-  xScale = d3
+  let xScale = d3
     .scaleLinear()
     .domain([0, d3.max(end_locations)])
     .range([0, width]);
@@ -45,8 +47,8 @@ function drawMap(genome) {
     .data(genome)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => (xScale(d.start_location)))
-    .attr("y", (d, i) => i%2 == 0 ? 0 : 40)
+    .attr("x", (d, i) => xScale(d.start_location))
+    .attr("y", (d, i) => (i % 2 == 0 ? 0 : 40))   //offset to distinguish overlapping genes
     .attr("width", d => xScale(d.length))
     .attr("height", "30px")
     .style("fill", d => colorScale(d.end_location))
@@ -54,7 +56,7 @@ function drawMap(genome) {
     .style("opacity", 0.5);
   // .call(d3.drag().on("drag"));
 
-  let xAxis = d3.axisBottom(xScale).ticks(100)
+  let xAxis = d3.axisBottom(xScale).ticks(100);
 
   svg
     .append("g")
