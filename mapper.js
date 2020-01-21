c3f179d173526b7b7cde17e67272a1bd0e24c6d9
James Eapen <jpe4@students.calvin.edu>
2020-01-21 10:28:33 -0500

    corrected scale, colors, axes

--- mapper.js
+++ mapper.js
@@ -1,67 +1,70 @@
-const margin = { top: 20, right: 10, bottom: 20, left: 30 };
+const margin = { top: 50, right: 50, bottom: 50, left: 30 };
 
 const width = 5000;
-const height = 200;
+const height = 50;
 
 const svg = d3.select("svg#map-area");
 
 svg
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
-  // .append("g")
+  .append("g")
+  .attr("id", "plot-area")
   .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
   .style("background-color", "skyblue");
 
 let loadingIndicator = d3.select("h4#loading-indicator").text("Load file");
 
-d3.json("gene_jsons/Barb_flat.json").then(function(genome) {
+d3.json("gene_jsons/sequence.json").then(function(genome) {
   loadingIndicator.text("File loaded");
   console.log("Loaded");
+  console.log(genome);
   drawMap(genome);
 });
 
 console.log("Loading");
 loadingIndicator.text("Loading");
-
+let xScale;
 function drawMap(genome) {
   let start_locations = genome.map(d => d.start_location);
   let end_locations = genome.map(d => d.end_location);
   let gene_lengths = genome.map(d => d.length);
   let products = genome.map(d => d.product);
 
-  let xScale = d3
+  xScale = d3
     .scaleLinear()
-    .domain([0, d3.max(end_locations)])
-    .range(0, 5000);
+    .domain([0, d3.max(genome, d => d.end_location)])
+    .range([0, width]);
 
-  console.log(genome.map(d => d.start_location))
-  svg
+  let colorScale = d3
+    .scaleSequential()
+    .domain([0, d3.max(end_locations)])
+    .interpolator(d3.interpolateRainbow);
+
+  d3.select("g#plot-area")
     .selectAll("rect")
     .data(genome)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => i * d.length)
+    .on("mouseover", function() {
+      console.log(d => d.product);
+    })
+    .attr("x", (d, i) => (i) * xScale(d.length))
     .attr("y", 0)
-    .attr("width", d => d.length)
+    .attr("width", d => xScale(d.length))
     .attr("height", "30px")
-    .style("fill", "transparent")
+    .style("fill", d => colorScale(d.end_location))
     .style("stroke", "black")
     .style("opacity", 0.5);
+  // .call(d3.drag().on("drag"));
 
-  // svg.selectAll("circle")
-  // .data(genome)
-  // .enter()
-  // .append("circle")
-  // .attr("cx", d => d.start_locations)
-  // .attr("cy", 25)
-  // .attr("r", 5)
-  // .style("fill", "skyblue")
-  // .style("opacity", "0.2")
+  console.log(genome.map(d => d.start_location));
+  let xAxis = d3.axisBottom(xScale).ticks(100)
 
-  let xAxis = d3.axisBottom(xScale);
-
+  console.log("transform", "translate(0, " + height + ")");
   svg
     .append("g")
-    .attr("transform", "translate(" + 0 + ", " + height + ")")
+    .attr("id", "xaxis")
+    .attr("transform", "translate(30, " + (height + margin.top) + ")")
     .call(xAxis);
 }
